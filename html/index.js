38dc5c345c70d548f7906a047df287c46fd1657f
Alexej Harm <alexej.h@xiphos.de>
2020-02-05 18:49:34 +0100

    handle unsupported browsers

--- html/index.js
+++ html/index.js
@@ -1,6 +1,9 @@
 import "@babel/polyfill";
 import main from "/main.svelte";
 
+const warning = document.getElementById("warning");
+warning.parentNode.removeChild(warning);
+
 const root = new main({ target: document.body });
 
 export default root;
